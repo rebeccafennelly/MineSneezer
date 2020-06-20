19bc9ff89623f9b1427f329c1ad6bdff5f0a6fa9
rebeccafennelly <64370782+rebeccafennelly@users.noreply.github.com>
2020-06-20 12:12:35 +0100

    finished for now

--- app.js
+++ app.js
@@ -17,117 +17,118 @@ const getRandomNumber = () => {
     return randomNumber;
   }
   return getRandomNumber();
-
 }
   
-  const createAtRandom = (type) => { 
+const createAtRandom = (type) => { 
   let randomNumber = getRandomNumber();
   occupiedCells.push(randomNumber);
-  // const cellArray = document.getElementsByClassName("cell");
   console.log(cellArray);
   let cell = cellArray[randomNumber];
   cell.classList.add(type);
 }
 
 
-endOfGame = () => {
+const endOfGame = () => {
   const endGamePopUp = document.querySelector(".endgamepopup");
+  const cells = document.querySelectorAll(".cell");
   endGamePopUp.classList.add("over");
-  resetGrid();
+  cells.forEach(cell => cell.removeEventListener("click", clickedClassFunction));
+  const soundUrl = "./sounds/trombone.mp3";
+  new Audio(soundUrl).play();
 }    
 
 
 const clickedClassFunction = (event) => {
 
-  event.target.classList.add("clicked");  
+  if(event.target.classList.contains("clicked") === false) {
+    event.target.classList.add("clicked");  
 
-  if (event.target.classList.contains("facemask")){ 
-    event.target.innerHTML = facemask;
-    (score += 5);
-    getInsideProgressUpdate.innerHTML = ("Who knows if they make a difference, or if the government approves of them this week, but you've won a face mask!");
-    document.getElementById("score").innerHTML = (score);
- 
-  }
-  if (event.target.classList.contains ("antibac")){
-    event.target.innerHTML = antibac;
-    (score += 10);
-    getInsideProgressUpdate.innerHTML = ("Well done, you have won some Antibacterial Hand Sanitiser. Slap it on and hide it in the darkest depths of your bag!");
-    document.getElementById("score").innerHTML = (score);
-  }
-  if (event.target.classList.contains ("looroll")){
-    event.target.innerHTML = looroll;
-    (score += 15);
-    getInsideProgressUpdate.innerHTML = ("Congratulations, you have won a roll of toilet paper!! All I’m saying is make it last!"); 
-    document.getElementById("score").innerHTML = (score);
-  }
-
-  if (event.target.classList.contains ("virus")){
-    event.target.innerHTML = (virus);
-    (score = 0);
-   document.getElementById("score").innerHTML = (score);
-  //  cell.innerHTML = ""
-    endOfGame();
-
-}
+    if (event.target.classList.contains("facemask")){ 
+      event.target.innerHTML = facemask;
+      (score += 5);
+      getInsideProgressUpdate.innerHTML = ("Who knows if they make a difference, or if the government approves of them this week, but you've won a face mask!");
+      document.getElementById("score").innerHTML = (score);
   
-  if (event.target.classList.contains ("blank")){ 
-    event.target.innerHTML = blank;
-    getInsideProgressUpdate.innerHTML = ("Blank! Sitting duck...");
-  }
+    }
+    if (event.target.classList.contains("antibac")){
+      event.target.innerHTML = antibac;
+      (score += 10);
+      getInsideProgressUpdate.innerHTML = ("Well done, you have won some Antibacterial Hand Sanitiser. Slap it on and hide it in the darkest depths of your bag!");
+      document.getElementById("score").innerHTML = (score);
+    }
+    if (event.target.classList.contains("looroll")){
+      event.target.innerHTML = looroll;
+      (score += 15);
+      getInsideProgressUpdate.innerHTML = ("Congratulations, you have won a roll of toilet paper!! All I’m saying is make it last!"); 
+      document.getElementById("score").innerHTML = (score);
+    }
 
+    if (event.target.classList.contains("virus")){
+      event.target.innerHTML = (virus);
+      (score = 0);
+    document.getElementById("score").innerHTML = (score);
+      endOfGame();
+
+  }
+    
+    if (event.target.classList.contains("blank")){ 
+      event.target.innerHTML = blank;
+      getInsideProgressUpdate.innerHTML = ("Blank! Sitting duck...");
+    }
+  }
 }
 
 
   const addEventListeners = () => {
     const cells = document.querySelectorAll(".cell");
-    cells.forEach (cell => cell.addEventListener ("click", clickedClassFunction));
+    cells.forEach (cell => cell.addEventListener("click", clickedClassFunction));
     };
-    addEventListeners();
+
   
-    // addEventListeners();
 
 
 
-  const resetGrid = () => {
-
-
-    // if (event.target.classList.contains("clicked")){
-    //   cell.removeEventListener("click");
-    //  }
+  const startGame = () => {
+      const endGamePopUp = document.querySelector(".endgamepopup");
 
       [...cellArray].forEach((cell) => {
       cell.classList.remove("facemask", "antibac", "looroll", "virus", "blank", "clicked");
+      cell.innerHTML = "";
       })
       score = 0; 
+      document.getElementById("score").innerHTML = (score);
       occupiedCells = [];
 
-    for (let index = 0; index < 3; index++) {
-
-      createAtRandom("virus");
+    for (let index = 0; index < 4; index++) {
       createAtRandom("facemask");
+    }
+
+    for (let index = 0; index < 3; index++) {
       createAtRandom("antibac");
     }
+
     for (let index = 0; index < 2; index++) {
         createAtRandom("looroll");
+        createAtRandom("virus");
         }
+
     for (let index = 0; index < 5; index++) {
           createAtRandom("blank");
         }
-    }
-  
-
-
+    
+    endGamePopUp.classList.remove("over");
+    addEventListeners();
+}
 
 const newGame = document.querySelector(".newgame");
-newGame.addEventListener("click", resetGrid);
+newGame.addEventListener("click", startGame);
+
+startGame();
 
 
-resetGrid();
-
-
-
- 
-
+const clickHere = document.querySelector(".clickhere");
+clickHere.addEventListener("click", startGame);
+// .addClass('hide');
 
 
 //  CLICK A virus, POP UP SAYS YOURE DEAD, REMOVE CLASSES ON CELLS APART FROM CLASS OF CELL, THEN REDISTRUBUTE.
@@ -140,11 +141,5 @@ resetGrid();
 //       score = 0; 
 //       occupiedCells = [];
 // }    
-
-
-
-const clickHere = document.querySelector(".clickhere");
-clickHere.addEventListener("click", resetGrid);
-endGamePopUp.classList.remove("over");
 
 
